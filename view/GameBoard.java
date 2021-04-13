39ff85d9cb5ef51acfa884abd1d0ebc7491305e3
davidrgb <davidrussellrgb@gmail.com>
2021-04-12 19:14:16 -0500

    Successful snake render

--- view/GameBoard.java
+++ view/GameBoard.java
@@ -1,17 +1,26 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import controller.ButtonClickListener;
+import controller.TimerListener;
+import model.Snake;
 
 public class GameBoard {
 
     public static final int WIDTH = 600;
     public static final int HEIGHT = 400;
+    public static final int CELL_SIZE = 20;
+    public static final int FPS = 4;
+    public static final int DELAY = 1000 / FPS;
     
     private JFrame window;
     private MyCanvas canvas;
@@ -19,6 +28,9 @@ public class GameBoard {
     private JButton stopButton = new JButton("Stop");
     private JButton exitButton = new JButton("Exit");
     private JLabel scoreDisplay = new JLabel();
+    private Timer timer;
+
+    private Snake snake = new Snake();
 
     public GameBoard(JFrame window) {
         this.window = window;
@@ -41,5 +53,35 @@ public class GameBoard {
         southPanel.add(stopButton);
         southPanel.add(exitButton);
         cp.add(BorderLayout.SOUTH, southPanel);
+
+        Text t1 = new Text("Click <Start> to Play", 100, 100);
+        t1.color = Color.yellow;
+        canvas.getFigures().add(t1);
+
+        ButtonClickListener buttonListener = new ButtonClickListener(this);
+        startButton.addActionListener(buttonListener);
+
+        timer = new Timer(DELAY, new TimerListener(this));
+        timer.start();
+    }
+
+    public MyCanvas getCanvas() {
+        return canvas;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public JButton getStartButton() {
+        return startButton;
+    }
+    
+    public JButton getStopButton() {
+        return stopButton;
+    }
+
+    public JButton getExitButton() {
+        return exitButton;
     }
 }
--- view/MyCanvas.java
+++ view/MyCanvas.java
@@ -5,11 +5,16 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
+
+import model.GameElement;
 
 public class MyCanvas extends JPanel {
     
     private GameBoard gameBoard;
+    private ArrayList<GameElement> figures = new ArrayList<>();
 
     public MyCanvas(GameBoard gameBoard, int width, int height) {
         this.gameBoard = gameBoard;
@@ -22,5 +27,13 @@ public class MyCanvas extends JPanel {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+
+        for (var f: figures) {
+            f.render(g2);
+        }
+    }
+
+    public ArrayList<GameElement> getFigures() {
+        return figures;
     }
 }
